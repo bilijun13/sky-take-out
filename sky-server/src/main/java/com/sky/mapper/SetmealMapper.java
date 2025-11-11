834daaddfd6b220b412868c06fd85bd2b4b6f4a3
bilijun13 <2825020416@qq.com>
2025-11-11 18:34:49 +0800

    修改套餐业务代码开发

--- sky-server/src/main/java/com/sky/mapper/SetmealMapper.java
+++ sky-server/src/main/java/com/sky/mapper/SetmealMapper.java
@@ -49,4 +49,7 @@ public interface SetmealMapper {
      */
     @Delete("delete from setmeal where id = #{id}")
     void deleteById(Long setmealId);
+
+    @AutoFill(OperationType.UPDATE)
+    void update(Setmeal setmeal);
 }
